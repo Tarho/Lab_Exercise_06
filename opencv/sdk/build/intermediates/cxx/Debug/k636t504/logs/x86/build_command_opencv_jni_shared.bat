19c48876ef2a7cd2ece6ef1cda2827b40f4ea8e9
Tarho <20521935@gm.uit.edu.vn>
2024-06-06 09:38:45 +0700

    bug

--- opencv/sdk/build/intermediates/cxx/Debug/k636t504/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\20521\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\20521\\OneDrive\\Desktop\\final\\sdk\\.cxx\\Debug\\k636t504\\x86" ^
-  opencv_jni_shared
